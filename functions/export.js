33d9040a906c2b9774d2a429246e23441db1800e
Robin Stut <robin@voorhoede.nl>
2020-08-31 14:55:06 +0200

    testing nlp function

---
+++ functions/export.js
@@ -0,0 +1,21 @@
+const { NlpManager } = require("node-nlp");
+const manager = new NlpManager({ languages: ["en"], threshold: 0.90 });
+
+// I suggest you train each intent with about 10 input examples.
+manager.addDocument("en", "Can I get a triple espresso please?", "Order");
+manager.addDocument("en", "Can I order a triple espresso please?", "Order");
+manager.addDocument("en", "Give me a triple espresso.", "Order");
+manager.addDocument("en", "I want a triple espresso.", "Order");
+manager.addDocument("en", "One triple espresso please.", "Order");
+// etc
+
+exports.handler = function (event, context, callback) {
+    await manager.train();
+    const minified = true
+    const data = manager.export(!minified);
+
+    callback(null, {
+        statusCode: 200,
+        body: data,
+    });
+};
