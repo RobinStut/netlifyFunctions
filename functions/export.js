aebdb0dd67d43a7e99a75409d8abb0bf44b842c9
Robin Stut <robin@voorhoede.nl>
2020-08-31 15:03:24 +0200

    read-only fix

--- functions/export.js
+++ functions/export.js
@@ -1,13 +1,13 @@
 const { NlpManager } = require("node-nlp");
 const manager = new NlpManager({ languages: ["en"], threshold: 0.90 });
 
-// I suggest you train each intent with about 10 input examples.
-manager.addDocument("en", "Can I get a triple espresso please?", "Order");
-manager.addDocument("en", "Can I order a triple espresso please?", "Order");
-manager.addDocument("en", "Give me a triple espresso.", "Order");
-manager.addDocument("en", "I want a triple espresso.", "Order");
-manager.addDocument("en", "One triple espresso please.", "Order");
-// etc
+// // I suggest you train each intent with about 10 input examples.
+// manager.addDocument("en", "Can I get a triple espresso please?", "Order");
+// manager.addDocument("en", "Can I order a triple espresso please?", "Order");
+// manager.addDocument("en", "Give me a triple espresso.", "Order");
+// manager.addDocument("en", "I want a triple espresso.", "Order");
+// manager.addDocument("en", "One triple espresso please.", "Order");
+// // etc
 
 exports.handler = async function (event, context, callback) {
     await manager.train();
