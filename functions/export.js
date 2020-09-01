9ffe38563fa344c03d891a7d5fcdec8bd234d388
Robin Stut <robin@voorhoede.nl>
2020-09-01 14:56:38 +0200

    feat: export

--- functions/export.js
+++ functions/export.js
@@ -1,105 +1,112 @@
 const { NlpManager } = require("node-nlp");
-// const manager = new NlpManager({ languages: ["en"], threshold: 0.90 });
+const firebase = require("firebase/app");
+require("firebase/database");
 
-// // I suggest you train each intent with about 10 input examples.
-// manager.addDocument("en", "Can I get a triple espresso please?", "Order");
-// manager.addDocument("en", "Can I order a triple espresso please?", "Order");
-// manager.addDocument("en", "Give me a triple espresso.", "Order");
-// manager.addDocument("en", "I want a triple espresso.", "Order");
-// manager.addDocument("en", "One triple espresso please.", "Order");
-// // etc
+const firebaseConfig = {
+    apiKey: `${process.env.apiKey}`,
+    authDomain: `${process.env.id}.firebaseapp.com`,
+    databaseURL: `https://${process.env.id}.firebaseio.com`,
+    projectId: `${process.env.id}`,
+    storageBucket: `${process.env.id}.appspot.com`,
+    messagingSenderId: `${process.env.messagingSenderId}`,
+    appId: `${process.env.appId}`
+};
 
-exports.handler = async function (event, context, callback) {
+const base = firebase.initializeApp(firebaseConfig);
+const db = base.database();
 
-    const subject = event.queryStringParameters.modal
-
-    console.log(subject);
-    // await manager.train();
-    const minified = true
-
-    const testData = {
+const testData = {
+    "settings": {
+        "languages": [
+            "en"
+        ],
+        "threshold": 0.9,
+        "tag": "nlp",
+        "autoLoad": true,
+        "autoSave": true,
+        "modelFileName": "model.nlp",
+        "calculateSentiment": true
+    },
+    "nluManager": {
         "settings": {
-            "languages": [
-                "en"
-            ],
-            "threshold": 0.9,
-            "tag": "nlp",
-            "autoLoad": true,
-            "autoSave": true,
-            "modelFileName": "model.nlp",
-            "calculateSentiment": true
+            "tag": "nlu-manager"
         },
-        "nluManager": {
-            "settings": {
-                "tag": "nlu-manager"
-            },
-            "locales": [
-                "en"
-            ],
-            "languageNames": {},
-            "domainManagers": {
-                "en": {
-                    "settings": {
-                        "locale": "en",
-                        "tag": "domain-manager-en",
-                        "nluByDomain": {
-                            "default": {
-                                "className": "NeuralNlu",
-                                "settings": {}
-                            }
-                        },
-                        "trainByDomain": false,
-                        "useStemDict": true
+        "locales": [
+            "en"
+        ],
+        "languageNames": {},
+        "domainManagers": {
+            "en": {
+                "settings": {
+                    "locale": "en",
+                    "tag": "domain-manager-en",
+                    "nluByDomain": {
+                        "default": {
+                            "className": "NeuralNlu",
+                            "settings": {}
+                        }
                     },
-                    "stemDict": {},
-                    "intentDict": {},
-                    "sentences": [],
-                    "domains": {
-                        "master_domain": {
-                            "settings": {
-                                "locale": "en",
-                                "tag": "nlu-en",
-                                "keepStopwords": true,
-                                "nonefeatureValue": 1,
-                                "nonedeltaMultiplier": 1.2,
-                                "spellCheck": false,
-                                "spellCheckDistance": 1,
-                                "filterZeros": true,
-                                "log": true
-                            }
+                    "trainByDomain": false,
+                    "useStemDict": true
+                },
+                "stemDict": {},
+                "intentDict": {},
+                "sentences": [],
+                "domains": {
+                    "master_domain": {
+                        "settings": {
+                            "locale": "en",
+                            "tag": "nlu-en",
+                            "keepStopwords": true,
+                            "nonefeatureValue": 1,
+                            "nonedeltaMultiplier": 1.2,
+                            "spellCheck": false,
+                            "spellCheckDistance": 1,
+                            "filterZeros": true,
+                            "log": true
                         }
                     }
                 }
-            },
-            "intentDomains": {},
-            "extraSentences": []
+            }
         },
-        "ner": {
-            "settings": {
-                "tag": "ner",
-                "entityPreffix": "%",
-                "entitySuffix": "%"
-            },
-            "rules": {}
+        "intentDomains": {},
+        "extraSentences": []
+    },
+    "ner": {
+        "settings": {
+            "tag": "ner",
+            "entityPreffix": "%",
+            "entitySuffix": "%"
         },
-        "nlgManager": {
-            "settings": {
-                "tag": "nlg-manager"
-            },
-            "responses": {}
+        "rules": {}
+    },
+    "nlgManager": {
+        "settings": {
+            "tag": "nlg-manager"
         },
-        "actionManager": {
-            "settings": {
-                "tag": "action-manager"
-            },
-            "actions": {}
+        "responses": {}
+    },
+    "actionManager": {
+        "settings": {
+            "tag": "action-manager"
         },
-        "slotManager": {}
-    }
+        "actions": {}
+    },
+    "slotManager": {}
+}
 
+exports.handler = async function (event, context, callback) {
     // const manager = new NlpManager();
     // const manager = new NlpManager({ modelFileName: '/tmp/model.nlp' });
     const manager = new NlpManager({ autoSave: false });
+
+    // use to read data from firebase
+    db.ref('model').on('value', async (snap) => {
+        console.log(snap.val());
+        console.log('it works');
+        // manager.import(snap.val());
+    })
+
     manager.import(testData);
 
     manager.addDocument("en", "Can I get a triple espresso please?", "Order");
@@ -110,8 +117,8 @@ exports.handler = async function (event, context, callback) {
 
     manager.train()
 
+    const minified = true
     const data = await manager.export(!minified);
-
 
     callback(null, {
         statusCode: 200,
