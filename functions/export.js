f1c796431de9953a3d015d8545d768a2012a048d
Robin Stut <robin@voorhoede.nl>
2020-09-03 14:45:57 +0200

    feat(export.js): Update v3 to v4

--- functions/export.js
+++ functions/export.js
@@ -1,27 +1,7 @@
-const { NlpManager } = require("node-nlp");
-// const firebase = require("firebase/app");
-// require("firebase/database");
-
-// const firebaseConfig = {
-//     apiKey: `${process.env.apiKey}`,
-//     authDomain: `${process.env.id}.firebaseapp.com`,
-//     databaseURL: `https://${process.env.id}.firebaseio.com`,
-//     projectId: `${process.env.id}`,
-//     storageBucket: `${process.env.id}.appspot.com`,
-//     messagingSenderId: `${process.env.messagingSenderId}`,
-//     appId: `${process.env.appId}`
-// };
-
-// if (!firebase.apps.length) {
-//     firebase.initializeApp(firebaseConfig);
-// }
-
-
-
+// const { NlpManager } = require("node-nlp");
 const { google } = require('googleapis')
 const axios = require('axios')
-// const dotenv = require('dotenv')
-// dotenv.config()
+const { dockStart } = require('@nlpjs/basic');
 
 const {
     GOOGLE_CLIENT_EMAIL,
@@ -38,41 +18,9 @@ const jwt = new google.auth.JWT(GOOGLE_CLIENT_EMAIL, null, privateKey, scopes);
 
 exports.handler = async function (event, context) {
 
-    //     // NLP manager
-    //     const manager = new NlpManager({ autoSave: false });
-
-    //     // get the current nlp model from the database
-    //     const model = await firebase.database().ref('model').once('value').then(snap => {
-    //         return snap.val()
-    //     });
-
-    //     // parse the data from the database
-    //     const parsedModel = JSON.parse(model)
-
-    //     // import the parsed model
-    //     manager.import(parsedModel);
-
-    //     // add sentences to the manager for training
-    //     manager.addDocument("en", "Can I get a triple espresso please?", "Order");
-    //     manager.addDocument("en", "Can I order a triple espresso please?", "Order");
-    //     manager.addDocument("en", "Give me a triple espresso.", "Order");
-    //     manager.addDocument("en", "I want a triple espresso.", "Order");
-    //     manager.addDocument("en", "One triple espresso please.", "Order");
-
-    //     // train the AI / manager
-    //     manager.train()
-
-    //     const minified = true
-
-    //     // export the minified manager to json (already stringified)
-    //     const data = await manager.export(minified);
-
-    //     // upload the new nlp model to the database
-    //     firebase.database().ref('model').set(data);
-
-
     try {
 
+        // create Google API token
         const token = await new Promise((resolve, reject) => jwt.authorize(async (error, tokens) => {
             if (error) {
                 reject(new Error('Error making request to generate token'))
@@ -86,60 +34,37 @@ exports.handler = async function (event, context) {
             return null;
         }
 
+        // headers and token needed for firebase GET call 
         const headers = { Authorization: 'Bearer ' + token }
 
-        const {data} = await axios.get(apiUrl, { headers })
+        // Firebase GET call, destructured to data
+        const { data } = await axios.get(apiUrl, { headers })
 
         // NLP manager
-        const manager = new NlpManager({ autoSave: false });
+        const dock = await dockStart({ use: ['Basic'], autoSave: false });
 
-    //     // get the current nlp model from the database
-    //     const model = await firebase.database().ref('model').once('value').then(snap => {
-    //         return snap.val()
-    //     });
+        // Setup NLP
+        const nlp = dock.get('nlp');
 
-    //     // parse the data from the database
-    //     const parsedModel = JSON.parse(model)
-
-    //     // import the parsed model
-        manager.import(data);
-
-
-        // add sentences to the manager for training
-        manager.addDocument("en", "Can I get a triple espresso please?", "Order");
-        manager.addDocument("en", "Can I order a triple espresso please?", "Order");
-        manager.addDocument("en", "Give me a triple espresso.", "Order");
-        manager.addDocument("en", "I want a triple espresso.", "Order");
-        manager.addDocument("en", "One triple espresso please.", "Order");
-
-        // train the AI / manager
-        manager.train()
+        // Import data
+        nlp.import(data);
 
         const minified = true
 
-        // export the minified manager to json (already stringified)
-        const output = await manager.export(minified);
+        // export the minified manager to json
+        const output = await nlp.export(minified);
 
-        console.log({output});
-
+        // stringify the output of the manager
         const stringified = JSON.stringify(output)
 
-        console.log({stringified});
-        // const {data} = await axios.get(apiUrl, { headers })
+        // upload stringified items to firebase
         const sendToDb = await axios.put(apiUrl, stringified)
 
-
-
-        // axios interceptors
-        // axios .request . response
-
-        // return data
-
+        // return the data
         return {
             statusCode: 200,
-            body: data,
+            body: '',
         }
-        // return the data
     } catch (e) {
         // throw new Error('Error fetching Google Analytics data')
         return {
@@ -147,11 +72,4 @@ exports.handler = async function (event, context) {
             body: e.message
         }
     }
-    
-    // callback(null, {
-    //     statusCode: 200,
-    //     body: data,
-    // });
-
-
 };
