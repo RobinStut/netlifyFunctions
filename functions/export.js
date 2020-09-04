5b752687cd75541f2d5fbda2e152bf36d4b7e186
Robin Stut <robin@voorhoede.nl>
2020-09-04 10:59:17 +0200

    feat(export.js): NLP with V3 example

--- functions/export.js
+++ functions/export.js
@@ -1,7 +1,7 @@
-// const { NlpManager } = require("node-nlp");
+const { NlpManager } = require("node-nlp");
 const { google } = require('googleapis')
 const axios = require('axios')
-const { dockStart } = require('@nlpjs/basic');
+
 
 const {
     GOOGLE_CLIENT_EMAIL,
@@ -20,7 +20,6 @@ exports.handler = async function (event, context) {
 
     try {
 
-        // create Google API token
         const token = await new Promise((resolve, reject) => jwt.authorize(async (error, tokens) => {
             if (error) {
                 reject(new Error('Error making request to generate token'))
@@ -34,79 +33,55 @@ exports.handler = async function (event, context) {
             return null;
         }
 
-        // headers and token needed for firebase GET call 
         const headers = { Authorization: 'Bearer ' + token }
 
-        // Firebase GET call, destructured to data
-        const { data } = await axios.get(apiUrl, { headers })
+        // const { data } = await axios.get(apiUrl, { headers })
 
         // NLP manager
-        const dock = await dockStart({ use: ['Basic'], autoSave: false });
 
-        // Setup NLP
-        const nlp = dock.get('nlp');
+        const logfn = (status, time) => console.log(status, time);
+        // const manager = new NlpManager({ autoSave: false, languages: ["en"], nlu: { log: logfn } });
+        const manager = new NlpManager({ autoSave: false, languages: ["en"] });
 
-        // Import data
-        // nlp.import(data);
+        // parse the data from the database
+        // const parsedModel = JSON.parse(data)
 
+        manager.addDocument('en', 'goodbye for now', 'greetings.bye');
+        manager.addDocument('en', 'bye bye take care', 'greetings.bye');
+        manager.addDocument('en', 'okay see you later', 'greetings.bye');
+        manager.addDocument('en', 'bye for now', 'greetings.bye');
+        manager.addDocument('en', 'i must go', 'greetings.bye');
+        manager.addDocument('en', 'hello', 'greetings.hello');
+        manager.addDocument('en', 'hi', 'greetings.hello');
+        manager.addDocument('en', 'howdy', 'greetings.hello');
 
+        // Train also the NLG
+        manager.addAnswer('en', 'greetings.bye', 'Till next time');
+        manager.addAnswer('en', 'greetings.bye', 'see you soon!');
+        manager.addAnswer('en', 'greetings.hello', 'Hey there!');
+        manager.addAnswer('en', 'greetings.hello', 'Greetings!');
 
-        nlp.addNamedEntityText("size", "grande", ["en"], ["Grande", "Large", "Triple"]);
-        nlp.addNamedEntityText("size", "short", ["en"], ["Short", "Small", "Single"]);
-        nlp.addNamedEntityText("size", "tall", ["en"], ["Tall", "Medium", "Double"]);
+        // train the AI / manager
+        manager.train()
 
-        nlp.addNamedEntityText("drink", "americano", ["en"], ["Americano", "Americanos"]);
-        nlp.addNamedEntityText("drink", "latte", ["en"], ["Latte", "Lattes"]);
-        nlp.addNamedEntityText("drink", "cappuccino", ["en"], ["Cappuccino", "Cappuccinos"]);
-
-        nlp.addDocument("en", "Can I get a %size% %drink% please?", "Order");
-        nlp.addDocument("en", "Can I order a %size% %drink% please?", "Order");
-        nlp.addDocument("en", "Give me a %size% %drink%.", "Order");
-        nlp.addDocument("en", "I want a %size% %drink%.", "Order");
-        nlp.addDocument("en", "One %size% %drink% please.", "Order");
-
-
-        // nlp.addLanguage('en');
-        // // Adds the utterances and intents for the NLP
-        // nlp.addDocument('en', 'goodbye for now', 'greetings.bye');
-        // nlp.addDocument('en', 'bye bye take care', 'greetings.bye');
-        // nlp.addDocument('en', 'okay see you later', 'greetings.bye');
-        // nlp.addDocument('en', 'bye for now', 'greetings.bye');
-        // nlp.addDocument('en', 'i must go', 'greetings.bye');
-        // nlp.addDocument('en', 'hello', 'greetings.hello');
-        // nlp.addDocument('en', 'hi', 'greetings.hello');
-        // nlp.addDocument('en', 'howdy', 'greetings.hello');
-
-        // // Train also the NLG
-        // nlp.addAnswer('en', 'greetings.bye', 'Till next time');
-        // nlp.addAnswer('en', 'greetings.bye', 'see you soon!');
-        // nlp.addAnswer('en', 'greetings.hello', 'Hey there!');
-        // nlp.addAnswer('en', 'greetings.hello', 'Greetings!');
-
-        await nlp.train()
-
-        const result = nlp.process("Hi, I'd like one triple espresso, please.")
-
-        console.log({ result });
-
+        // export the minified manager to json (already stringified)
         const minified = true
+        const output = await manager.export(minified);
+        manager.save();
 
-        // export the minified manager to json
-        const output = await nlp.export(minified);
+        const response = await manager.process('en', "okay see you later");
+        console.log(response);
+        // console.log({ output });
 
-        // console.log(output);
-
-        // stringify the output of the manager
         const stringified = JSON.stringify(output)
 
-        // upload stringified items to firebase
-        const sendToDb = await axios.put(apiUrl, stringified)
+        // const sendToDb = await axios.put(apiUrl, stringified)
 
-        // return the data
         return {
             statusCode: 200,
             body: '',
         }
+        // return the data
     } catch (e) {
         // throw new Error('Error fetching Google Analytics data')
         return {
