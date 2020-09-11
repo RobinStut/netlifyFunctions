f4a063e8a7c6cf71efd3d300234ff83545409402
Robin Stut <robin@voorhoede.nl>
2020-09-11 13:29:59 +0200

    feat(ask.js): updating Netlify Function

--- functions/ask.js
+++ functions/ask.js
@@ -8,7 +8,8 @@ const {
 } = process.env
 
 const privateKey = GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n')
-const apiUrl = 'https://graduate-16c74.firebaseio.com/trainedModel.json'
+const modelUrl = 'https://graduate-16c74.firebaseio.com/trainedModel.json'
+const trainingHistoryUrl = 'https://graduate-16c74.firebaseio.com/trainingHistory.json/'
 const scopes = [
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/firebase.database"
@@ -17,7 +18,6 @@ const jwt = new google.auth.JWT(GOOGLE_CLIENT_EMAIL, null, privateKey, scopes);
 
 exports.handler = async function (event, context) {
     const receivedMessage = event.queryStringParameters.message
-
     try {
 
         // create Google API token
@@ -37,23 +37,58 @@ exports.handler = async function (event, context) {
         // headers and token needed for firebase GET call 
         const headers = { Authorization: 'Bearer ' + token }
 
+        // 1.   import het laatste nlp model.
         // Firebase GET call, destructured to data
-        const { data } = await axios.get(apiUrl, { headers })
+        const { data: trainingHistory = data } = await axios.get(trainingHistoryUrl, { headers })
+        const trainingData = Object.values(trainingHistory).map(data => data.trainingData)
 
         // NLP manager
         const dock = await dockStart({ use: ['Basic'], autoSave: false });
-
-        // // Setup NLP
         const nlp = dock.get('nlp');
 
-        // // Import data
-        nlp.import(data);
+        // // Setup NLP
+
+        nlp.addLanguage('nl');
+
+        // 2.   ontvang nieuwe input voor het model. (vragen en antwoorden)
+
+        const chatbotAnswerFallBack = []
+
+        trainingData.forEach(chatbotOrUser => {
+            for (const [key, value] of Object.entries(chatbotOrUser)) {
+                value.forEach(input => {
+                    if (!input) return
+                    const { language, intent, utterance } = input
+                    const chatbotOutput = key === 'chatbotReactionTrainingForm'
+
+                    // 3.2  Zo nee, train model
+                    if (chatbotOutput) {
+                        chatbotAnswerFallBack.push({ language, intent, utterance })
+                        nlp.addAnswer(language, intent, utterance);
+
+                    }
+                    else {
+                        nlp.addDocument(language, utterance, intent);
+                    }
+                });
+            }
+        })
+
+        await nlp.train()
 
         const result = await nlp.process('nl', receivedMessage)
+        const { intent, answers } = result
+
+        if (!answers.length && intent !== 'None') {
+            const fallbackAnswer = chatbotAnswerFallBack.find(({ intent }) => intent === intent)
+            if (fallbackAnswer) answers.push(fallbackAnswer.utterance)
+        }
+
+        if (intent === 'None') answers.push('Ik ben niet goed genoeg getrained om deze vraag te beantwoorden...')
 
         return {
             statusCode: 200,
-            body: JSON.stringify(result)
+            body: JSON.stringify({ 'result': { intent, answers }, 'statusCode': 200 })
         }
     }
     catch (e) {
